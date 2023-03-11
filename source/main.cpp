a6bac35374e60385f6715f0d0e7f107c675eb79f
Rhiannon <renii@reniivali.com>
2023-03-11 15:22:13 -0700

    other misc improvments and now it works better

--- source/main.cpp
+++ source/main.cpp
@@ -26,14 +26,14 @@ typedef struct
 
 int clickUpPrice = 15;
 int clickUpOwn = 0;
-int clicks = 0;
-int CPC = 1;
-int CPS = 0;
+unsigned long long int clicks = 0;
+unsigned long long int CPC = 1;
+unsigned long long int CPS = 0;
 int cpsTimer = 0;
 int clickerprice = 50;
 int clickerown = 0;
-int clickerUprice = 1000;
-int clickerUown = 1;
+unsigned long long int clickerUprice = 1000;
+unsigned long long int clickerUown = 1;
 int clickm2price = 500;
 int clickm2own = 0;
 int frame = 0;
@@ -42,6 +42,8 @@ int buyscreen = 1;
 int buytime = 0;
 bool buy1 = false;
 bool buy2 = false;
+bool controls = true;
+bool DEBUG = false;
 
 static C2D_SpriteSheet spriteSheet;
 static Sprite sprites[100];
@@ -121,27 +123,66 @@ int main(int argc, char **argv)
 
 		if (kDown & KEY_START) break; // break in order to return to hbmenu
 
+		if (kDown & KEY_SELECT) {
+			if (DEBUG) DEBUG = false; else DEBUG = true;
+		}
+
 		//Read the touch screen coordinates and add them to a variable
 		touchPosition touch;
 		hidTouchRead(&touch);
 
-		//shop page switching code
-		if (kDown & KEY_ZR) {
-			buyscreen += 1;
-			if (buyscreen > 2) {
-				buyscreen = 1;
+		//buttons
+		if (DEBUG) {
+			if (kDown & KEY_ZR) {
+				buyscreen += 1;
+				if (buyscreen > 2) {
+					buyscreen = 1;
+				}
 			}
-		}
-		if (kDown & KEY_ZL) {
-			buyscreen -= 1;
-			if (buyscreen < 1) {
-				buyscreen = 2;
+			if (kDown & KEY_ZL) {
+				buyscreen -= 1;
+				if (buyscreen < 1) {
+					buyscreen = 2;
+				}
+			}
+			//add clickers
+			if (kDown & KEY_A) { clickerown += 100; }
+			//add clicker multiplier
+			if (kDown & KEY_B) { clickerUown += 1; }
+			//cpc ++++
+			if (kDown & KEY_X) { CPC += 100; }
+			//literally just multiply values
+			if (kDown & KEY_Y) { CPC *= 2; clickerUown *= 2; clickerown *= 2; }
+			//speedy click button
+			if (kHeld & KEY_R) { clicks += CPC; }
+		} else {
+			//controls screen switch
+			if (kDown & KEY_X) {
+				if (controls) controls = false; else controls = true;
+			}
+			// buy screen switcher
+			if (kDown & KEY_ZR || kDown & KEY_DRIGHT) {
+				buyscreen += 1;
+				if (buyscreen > 2) {
+					buyscreen = 1;
+				}
+			}
+			if (kDown & KEY_ZL || kDown & KEY_DLEFT) {
+				buyscreen -= 1;
+				if (buyscreen < 1) {
+					buyscreen = 2;
+				}
+			}
+
+			// click buttons
+			if (kDown != kDownOld || kHeld != kHeldOld || kUp != kUpOld) {
+				if (kDown & KEY_A || kDown & KEY_L || kDown & KEY_R) { clicks += CPC; }
 			}
 		}
 
 		//figure out if the buy buttons are getting pressed
-		if (touch.px > 20 && touch.px < 150 && touch.py > 40 && touch.py < 220) buy1 = true; else buy1 = false;
-		if (touch.px > 170 && touch.px < 300 && touch.py > 40 && touch.py < 220) buy2 = true; else buy2 = false;
+		if (touch.px > 5 && touch.px < 155 && touch.py > 40 && touch.py < 235) buy1 = true; else buy1 = false;
+		if (touch.px > 165 && touch.px < 315 && touch.py > 40 && touch.py < 235) buy2 = true; else buy2 = false;
 		if (buytime >= 1) buytime -= 1;
 
 		//purchasing code
@@ -152,32 +193,32 @@ int main(int argc, char **argv)
 				clickUpPrice += 5;
 				CPC += 1;
 				clickUpOwn++;
-				buytime = 30;
+				buytime = 5;
 			}
 			//clicker upgrade
 			if (clicks >= clickerUprice && buyscreen == 2) {
 				clicks -= clickerUprice;
-				clickerUprice = clickerUprice*2;
+				clickerUprice = clickerUprice*1.1f;
 				clickerUown += 1;
-				buytime = 30;
+				buytime = 5;
 			}
 		}
 
 		if (buy2 && buytime == 0) {
-			//clicker
-			if (clicks >= clickerprice && buyscreen == 1) {
-				clicks -= clickerprice;
-				clickerprice += 25;
-				clickerown += 1;
-				buytime = 30;
-			}
 			//click upgrade, mk2
-			if (clicks >= clickm2price && buyscreen == 2) {
+			if (clicks >= clickm2price && buyscreen == 1) {
 				clicks -= clickm2price;
 				clickm2price += 100;
 				clickm2own++;
 				CPC += 10;
-				buytime = 30;
+				buytime = 5;
+			}
+			//clicker
+			if (clicks >= clickerprice && buyscreen == 2) {
+				clicks -= clickerprice;
+				clickerprice += 25;
+				clickerown += 1;
+				buytime = 5;
 			}
 		}
 
@@ -191,25 +232,28 @@ int main(int argc, char **argv)
 				clicks += CPS;
 			} else {cpsTimer -= 1;}
 		}
-		
-		//update clicks (console)
-		//consoleSelect(&top);
-		//printf("\x1b[1;1HClicks: %d\n", clicks);
 
-		if (kDown != kDownOld || kHeld != kHeldOld || kUp != kUpOld)
-		{
-			if (kDown & KEY_A || kDown & KEY_L || kDown & KEY_R) {clicks += CPC;}
-		}
-
-		//render text scene
+		//render scenes
 		C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
 		C2D_TargetClear(top, C2D_Color32(0x1E, 0x1E, 0x2E, 0xFF));
 		C2D_SceneBegin(top);
 
 		u32 col = 0xCDD6F4FF;
 		//main CPS / CPC display
-		drawDynamicText(g_dynBuf, 200.0f, 10.0f, 1.0f, col, font, C2D_AlignCenter, "Clicks %u\nCPS: %i\nCPC: %i", clicks, CPS, CPC);
-		drawDynamicText(g_dynBuf, 5.0f, 75.0f, 1.0f, col, font, C2D_AlignLeft, "Misc:\nFrame: %i\nCPS Timer: %i\nBuy Cooldown: %i", frame, cpsTimer, buytime);
+		drawRect(10, 10, 380, 80, 5, C2D_Color32(0x18, 0x18, 0x28, 0xFF), C2D_Color32(0x58, 0x5B, 0x70, 0xFF));
+		drawDynamicText(g_dynBuf, 20.0f, 15.0f, 1.0f, col, font, C2D_AlignLeft, "Clicks: %llu\nCPS   : %llu\nCPC   : %llu", clicks, CPS, CPC);
+
+		drawRect(10, 100, 320, 120, 5, C2D_Color32(0x18, 0x18, 0x28, 0xFF), C2D_Color32(0x58, 0x5B, 0x70, 0xFF));
+		if (controls) {
+			drawDynamicText(g_dynBuf, 20.0f, 105.0f, 1.0f, col, font, C2D_AlignLeft, "Controls:\nA/L/R: Click\nX: Toggle Control Help\nZL/ZR/D-Pad: Switch Shop Page\nStart: Exit");
+		} else {
+			if (DEBUG) {
+				drawDynamicText(g_dynBuf, 20.0f, 105.0f, 1.0f, col, font, C2D_AlignLeft, "Add Clickers: A\nAdd Clicker Multi: B\nAdd CPC: X\nMult values by 2: Y\nFast Click: R");
+				drawDynamicText(g_dynBuf, 5.0f, 220.0f, 0.7f, col, font, C2D_AlignLeft, "DEBUG ENABLED | Frame: %i", frame);
+			} else {
+				drawDynamicText(g_dynBuf, 20.0f, 105.0f, 1.0f, col, font, C2D_AlignLeft, "Misc:\nFrame: %i\nBuy Cooldown: %i", frame, buytime);
+			}
+		}
 
 		//bottom
 		C2D_SceneBegin(bottom);
@@ -218,28 +262,25 @@ int main(int argc, char **argv)
 		//draw the buy buttons
 		//bord: C2D_Color32(0x58, 0x5B, 0x70, 0xFF);
 		//fill: C2D_Color32(0x18, 0x18, 0x25, 0xFF);
-		drawRect(20 , 40, 130, 180, 5, C2D_Color32(0x18, 0x18, 0x25, 0xFF), C2D_Color32(0x58, 0x5B, 0x70, 0xFF));
-		drawRect(170, 40, 130, 180, 5, C2D_Color32(0x18, 0x18, 0x25, 0xFF), C2D_Color32(0x58, 0x5B, 0x70, 0xFF));
+		drawRect(5 , 40, 150, 195, 5, C2D_Color32(0x18, 0x18, 0x25, 0xFF), C2D_Color32(0x58, 0x5B, 0x70, 0xFF));
+		drawRect(165, 40, 150, 195, 5, C2D_Color32(0x18, 0x18, 0x25, 0xFF), C2D_Color32(0x58, 0x5B, 0x70, 0xFF));
+		//5 + 145
 
 		//draw the buy buttons' text
 		switch (buyscreen) {
 			case 1:
-				drawDynamicText(g_dynBuf, 160.0f, 5.0f, 1.0f, col, font, C2D_AlignCenter, "Page 1");
-				drawDynamicText(g_dynBuf, 85.0f, 80.0f, 0.9f, col, font, C2D_AlignCenter, "CPC Upgrade\n+1 CPC\nPrice: %i\nPrice+: 5\nOwned: %i", clickUpPrice, clickUpOwn);
-				drawDynamicText(g_dynBuf, 235.0f, 80.0f, 0.9f, col, font, C2D_AlignCenter, "Clicker\n+1 CPS\nPrice: %i\nPrice+: 25\nOwned: %i", clickerprice, clickerown);
+				drawDynamicText(g_dynBuf, 160.0f, 5.0f, 1.0f, col, font, C2D_AlignCenter, "Shop: Page 1");
+				drawDynamicText(g_dynBuf, 80.0f, 50.0f, 0.9f, col, font, C2D_AlignCenter, "CPC Upgrade\n+1 CPC\nPrice: %i\nPrice+: 5\nOwned: %i", clickUpPrice, clickUpOwn);
+				drawDynamicText(g_dynBuf, 240.0f, 50.0f, 0.9f, col, font, C2D_AlignCenter, "CPC Mk.2\n+10 CPC\nPrice: %i\nPrice+: 100\nOwned: %i", clickm2price, clickm2own);
 				break;
 			case 2:
-				drawDynamicText(g_dynBuf, 160.0f, 5.0f, 1.0f, col, font, C2D_AlignCenter, "Page 2");
-				drawDynamicText(g_dynBuf, 85.0f, 80.0f, 0.9f, col, font, C2D_AlignCenter, "Clicker+\nPrice: %i\nPrice+: %i\nOwned: %i", clickerUprice, clickerUprice, clickerUown);
-				drawDynamicText(g_dynBuf, 235.0f, 80.0f, 0.9f, col, font, C2D_AlignCenter, "CPC Mk.2\nPrice: %i\nPrice+: 100\nOwned: %i", clickm2price, clickm2own);
+				drawDynamicText(g_dynBuf, 160.0f, 5.0f, 1.0f, col, font, C2D_AlignCenter, "Shop: Page 2");
+				drawDynamicText(g_dynBuf, 80.0f, 50.0f, 0.9f, col, font, C2D_AlignCenter, "Clicker+\nEach clicker\ngets +1 CPS\nPrice: %llu\nPrice+: %llu\nOwned: %llu", clickerUprice, clickerUprice/10, clickerUown-1);
+				drawDynamicText(g_dynBuf, 240.0f, 50.0f, 0.9f, col, font, C2D_AlignCenter, "Clicker\n+%i CPS\nPrice: %i\nPrice+: 25\nOwned: %i", clickerUown, clickerprice, clickerown);
 				break;
 		}
 
 		C3D_FrameEnd(0);
-
-		//copy our image into the bottom screen's frame buffer
-		//if (buyscreen == 1) memcpy(fbb, buy1_bgr, buy1_bgr_size);
-		//if (buyscreen == 2) memcpy(fbb, buy2_bgr, buy2_bgr_size);
 
 		//Set keys old values for the next frame
 		kDownOld = kDown;
